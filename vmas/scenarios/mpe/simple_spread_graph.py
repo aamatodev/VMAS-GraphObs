09764ec92335b78b982470192e997262f7f660de
Alessandro Amato <aa357@students.uwf.edu>
2025-03-18 11:25:55 -0500

    feat: set default value for worldsize to None

--- vmas/scenarios/mpe/simple_spread_graph.py
+++ vmas/scenarios/mpe/simple_spread_graph.py
@@ -18,8 +18,8 @@ class Scenario(BaseScenario):
         num_agents = kwargs.pop("n_agents", 3)
         obs_agents = kwargs.pop("obs_agents", True)
         ScenarioUtils.check_kwargs_consumed(kwargs)
-        x_semidim = kwargs.pop("x_semidim", 1)
-        y_semidim = kwargs.pop("y_semidim", 1)
+        x_semidim = kwargs.pop("x_semidim", None)
+        y_semidim = kwargs.pop("y_semidim", None)
         self.obs_agents = obs_agents
 
         world = World(batch_dim=batch_dim, device=device, x_semidim=x_semidim, y_semidim=y_semidim)
